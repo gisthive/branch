a808b322cbf140d1ade7fb2f4606713150ece95c
Edidiong Udoh <gisthive@gmail.com>
2019-02-14 23:19:17 +0100

    Account fixed

--- resources/views/pages/account.blade.php
+++ resources/views/pages/account.blade.php
@@ -4,8 +4,7 @@
 <div class="tile is-ancestor"style="padding: 10px" >
     <div class="tile is-parent is-vertical is-3">
       <article class="tile is-child box notification is-danger">
-        <p class="title">Profile here</p>
-        <p class="subtitle">Describe d shit here</p>
+        <p class="title">{{ Auth::user()->name }}</p>
         <div class="content">
           <!-- Content -->
         </div>
@@ -15,10 +14,25 @@
   <div class="tile is-parent is-vertical">
     <article class="tile is-child box notification is-success">
       <div class="content">
-        <p class="title">All d info here</p>
-        <p class="subtitle">More info down here</p>
+        <p class="title">Account & Delivery Info.</p>
         <div class="content">
-          <!-- Content -->
+          <div class="columns">
+            <div class="column">
+              <input class="input" placeholder="email" required/>
+              <input class="input" placeholder="Mobile number" required/>
+              <input class="input" placeholder="PostCode (optional)" name="postcode" />
+              <select class="input" name="country">
+                <option value="Nigeria"> Nigeria </option>
+              </select>
+              
+            </div>
+            <div class="column">
+              <input class="input" placeholder="Address 1" required/>
+              <input class="input" placeholder="Address 2 (optional)" />
+              <input class="input" placeholder="City (optional)" />
+              <input class="input" placeholder="State (optional)" />
+            </div>
+          </div>
         </div>
       </div>
     </article>
