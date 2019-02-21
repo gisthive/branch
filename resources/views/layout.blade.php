c7d405b7018600ff12bb7d2a94b06be6401bf095
Edidiong Udoh <gisthive@gmail.com>
2019-02-21 22:31:45 +0100

    interswitching

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,25 +11,33 @@
             <li><a href="/category/{{$cats['name']}} ">{{$cats['name']}}</a></li>
         @endforeach    
         </ul>
-    </div> 
+    </div>
+  <div class="searchBar">
+    @include ('includes.errors')
+    @include ('includes.success')
+  </div>
+   <section> 
+   <!-- hero component -->
+   <div>
+    @include ('includes.hero')
+   </div>  
+   <!-- end hero component -->
+   <!-- search bar -->
     <div class="is-hidden-desktop">
     <form class="searchBar" action="/search" method="POST">
     {{csrf_field()}}
         <input name="q" class="input is-rounded" type="search" placeholder="Find Drugs...">
     </form>
   </div>
-  <div class="searchBar">
-    @include ('includes.errors')
-    @include ('includes.success')
-  </div>
-   <section> 
-   <div><br>
-    @include ('includes.hero')
-   </div> 
+  <!-- end search bar -->
+  <!-- each page content -->
     <div class="container is-fluid">
         @yield('content')
     </div>    
+    <!-- end page content -->
 </section> 
+<!-- footer -->
     @include('includes.footer')
+    <!-- end footer -->
 </body>
 </html>
